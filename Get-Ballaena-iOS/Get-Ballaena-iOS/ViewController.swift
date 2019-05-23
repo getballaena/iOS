6ae2b3fb1b5a5c472e21edffda322016eac126f3
Woopa01 <whdnwls49@naver.com>
2019-05-23 19:01:26 +0900

    initial commit

---
+++ Get-Ballaena-iOS/Get-Ballaena-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get-Ballaena-iOS
+//
+//  Created by 조우진 on 23/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
