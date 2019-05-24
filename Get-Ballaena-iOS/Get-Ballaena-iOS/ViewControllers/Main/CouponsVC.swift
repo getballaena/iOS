0e7fa17303e27b70eee6ffc57fe4998e67cf5575
Woopa01 <whdnwls49@naver.com>
2019-05-24 15:13:28 +0900

    [ADD] ViewController files

---
+++ Get-Ballaena-iOS/Get-Ballaena-iOS/ViewControllers/Main/CouponsVC.swift
@@ -0,0 +1,15 @@
+//
+//  CouponsVC.swift
+//  Get-Ballaena-iOS
+//
+//  Created by 조우진 on 24/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CouponsVC: UIViewController {
+    override func viewDidLoad() {
+    }
+}
---
+++ Get-Ballaena-iOS/Get-Ballaena-iOS/ViewControllers/Main/StampTourVC.swift
@@ -0,0 +1,15 @@
+//
+//  StampTourVC.swift
+//  Get-Ballaena-iOS
+//
+//  Created by 조우진 on 24/05/2019.
+//  Copyright © 2019 조우진. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StampTourVC: UIViewController {
+    override func viewDidLoad() {
+    }
+}
