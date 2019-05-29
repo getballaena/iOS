c603c8f84ca8ee8b97e147b0fbb442ed54b119c5
Woopa01 <whdnwls49@naver.com>
2019-05-29 14:09:30 +0900

    [UPDATE] LoginViewModel bind code

--- Get-Ballaena-iOS/Get-Ballaena-iOS/ViewControllers/Login/LoginVC.swift
+++ Get-Ballaena-iOS/Get-Ballaena-iOS/ViewControllers/Login/LoginVC.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class LoginVC: UIViewController {
-
+    @IBOutlet weak var usernameInput: UITextField!
+    @IBOutlet weak var registerBtn: UIButton!
+    
+    var viewModel: LoginViewModel!
+    let disposeBag = DisposeBag()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UserDefaults.standard.set(UIDevice.current.identifierForVendor!.uuidString.lowercased(), forKey: "uuid")
+        bindViewModel()
+    }
+    
+}
+
+extension LoginVC {
+    func bindViewModel(){
+        viewModel = LoginViewModel()
+        
+        viewModel.uuid.accept(UserDefaults.standard.value(forKey: "uuid") as! String)
+        
+        usernameInput.rx.text
+            .orEmpty
+            .bind(to: viewModel.username)
+            .disposed(by: disposeBag)
+        
+        registerBtn.rx.tap
+            .bind(to: viewModel.registerDidClicked)
+            .disposed(by: disposeBag)
+        
+        viewModel.checkDone
+            .drive(onNext: { isSuccessed in
+                if isSuccessed {
+                    self.performSegue(withIdentifier: "toMain", sender: nil)
+                }
+            })
+        .disposed(by: disposeBag)
+        
+        viewModel.registerDone
+            .drive(onNext: { isSuccessed in
+                if isSuccessed {
+                    self.performSegue(withIdentifier: "toMain", sender: nil)
+                } else {
+                    self.showToast(msg: "잠시후 다시 시도해주세요.")
+                }
+            })
+        .disposed(by: disposeBag)
         
     }
-
-
 }
 
