96d8b28f54bd070f11936dd4e110b429771d13c7
Woopa01 <whdnwls49@naver.com>
2019-05-31 19:33:36 +0900

    [UPDATE] add Team Color

--- Get-Ballaena-iOS/Get-Ballaena-iOS/Util/Colors.swift
+++ Get-Ballaena-iOS/Get-Ballaena-iOS/Util/Colors.swift
@@ -10,11 +10,14 @@ import Foundation
 import UIKit
 
 enum Color {
-    case MAINBLUE
-    
+    case MAINBLUE, HUMPBACKWHALE, MINKWHALE, GREATWHALE
+
     func getColor() -> UIColor {
         switch self {
         case .MAINBLUE: return UIColor(red: 98/255, green: 185/255, blue: 211/255, alpha: 1)
+        case .HUMPBACKWHALE: return UIColor(red: 237/255, green: 100/255, blue: 95/255, alpha: 1)
+        case .MINKWHALE: return UIColor(red: 101/255, green: 190/255, blue: 86/255, alpha: 1)
+        case .GREATWHALE: return UIColor(red: 79/255, green: 84/255, blue: 237/255, alpha: 1)
         }
     }
 }
